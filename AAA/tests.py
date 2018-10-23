5462b61e5c7f16729ed4549cb6e7b21e981923f1
Jinhao Li <jinhao@Jinhaos-MacBook-Pro.local>
2018-10-23 14:47:38 +1000

    add tests

--- AAA/tests.py
+++ AAA/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
-
+from .views import *
 # Create your tests here.
+
+class ViewTestCase(TestCase):
+    def setUp(self):
+        pass
+    def test_get_paras(self):
+        """"""
+        self.assertEqual(len(get_paras(1)), 2)
+        self.assertEqual(len(get_paras(2)), 2)
+        self.assertEqual(len(get_paras(3)), 2)
+        # self.assertEqual(cat.speak(), 'The cat says "meow"')
